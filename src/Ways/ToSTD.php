8567ab6c8b17029d839473919afff59e050b8c7d
rulud <ruslem@list.ru>
2017-02-22 01:35:09 +0300

    Update ToSTD.php

--- src/Ways/ToSTD.php
+++ src/Ways/ToSTD.php
@@ -9,8 +9,7 @@ class ToSTD extends SetWay
 	public function log($level, $message, array $context = [])
 	{
 		$cont = $this->getDate()." ".$level." ".$message." ".$this->contextToString($context);
-		$stdout = fopen('php://stdout', 'w');
 		echo "STDOUT:\n";
-        fwrite($stdout , $cont);
+        	fwrite(STDOUT, $cont);
 	}
 }
